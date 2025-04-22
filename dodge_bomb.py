3ef3031a14bb20922ab4962c451f657e40a8b380
tsubasa <c0a2417498@edu.teu.ac.jp>
2025-04-22 15:04:14 +0900

    4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -51,6 +51,11 @@ def main():
                 return
         screen.blit(bg_img, [0, 0]) 
 
+        if kk_rct.colliderect(bb_rct):
+            print("game over")
+            return
+
+
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key,mv in DELTA.items():
